bb9946edcb809aed96fcbd7d9eb65cdb5fd2ffe5
ABHAY4321 <85047246+ABHAY4321@users.noreply.github.com>
2023-04-10 12:49:30 +0530

    Update 9. cmd_line_arg.sh

--- 9. cmd_line_arg.sh
+++ 9. cmd_line_arg.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# name=Abhay
+# age=28
 
-
-echo ${0}
-echo ${1}
-echo ${2}
-
+# echo ${0}
+# echo ${1}
+# echo ${2}
 name=${1}
 age=${2}
-
-echo "Hello ${name}, Your age is ${age}."
+echo "My name is ${name} and my age is ${age}."
+# ls -l
+# x =abhay                  ## Here it treats x as command & abhay as command line argument.
+# x= abhay                  ## Here it treats x as variable whose value is blank space & abhay as seperate command.
