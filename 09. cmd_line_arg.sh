a070eb486e1d4b626ed2913eea7c4c998d47df09
ABHAY4321 <85047246+ABHAY4321@users.noreply.github.com>
2023-07-28 12:30:56 +0530

    Update 09. cmd_line_arg.sh

--- 09. cmd_line_arg.sh
+++ 09. cmd_line_arg.sh
@@ -10,4 +10,4 @@ age=${2}
 echo "My name is ${name} and my age is ${age}."
 # ls -l
 # x =abhay                  ## Here it treats x as command & abhay as command line argument.
-# x= abhay                  ## Here it treats x as variable whose value is blank space & abhay as seperate command.
+# x= abhay                  ## Here it treats x as variable whose value is blank space & abhay as seperate command. So never use blank space in variable.
