e550fa469c8edaa20660c93c97f8c58fe535ec79
ABHAY4321 <85047246+ABHAY4321@users.noreply.github.com>
2023-07-28 12:17:07 +0530

    Update 08. read_variable.sh

--- 08. read_variable.sh
+++ 08. read_variable.sh
@@ -3,6 +3,7 @@
 read -p "Please enter your name: " name
 read -p "Please enter your age: " age
 read -p "Enter password: " -s password    # s -> secure
+echo      # This echo is used to give a space & print o/p in next line, else after asking for password, it will print entire o/p in same line. 
 
 #read            # Variable name is not given.
 #echo ${REPLAY}  # If we don't give variable name, then the variable value will get stored into REPLAY variable.
