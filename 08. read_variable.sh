325aa4c9125a0bd6f0fb3d10db1a92a4c6380045
ABHAY4321 <85047246+ABHAY4321@users.noreply.github.com>
2023-07-28 12:12:45 +0530

    Update 08. read_variable.sh

--- 08. read_variable.sh
+++ 08. read_variable.sh
@@ -4,6 +4,8 @@ read -p "Please enter your name: " name
 read -p "Please enter your age: " age
 read -p "Enter password: " -s password
 
-echo
+#read            # Variable name is not given.
+#echo ${REPLAY}  # If we don't give variable name, then the variable value will get stored into REPLAY variable.
+
 
 echo "Hello ${name}, Your age is ${age}. Your security credentilal is ${password}"
