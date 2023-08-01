34a96d44ff335a534204705ca52cdc397e683f4a
ABHAY4321 <85047246+ABHAY4321@users.noreply.github.com>
2023-08-01 07:04:42 +0530

    Update 12. read_only_varibale.sh

--- 12. read_only_varibale.sh
+++ 12. read_only_varibale.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 name="Abhay"
-#readonly name   #This will make this variable as read only. It can't be changed.
-echo "${name}"
+#readonly name    #This will make this variable as read only. It can't be changed.
+echo "${name}"    #o/p: Abhay 
+#name="Tom"       #Error: read_only_var.sh: line 6: name: readonly variable
+#echo "${name}"   #o/p: Abhay. If "name" is not readonly variable, then this o/p will become "Tom". 
 
-unset name
-
-#name="Singh"
-
-echo "${name}"
+unset name        # This will unset "name" variable. Alternate way to unset it: name=
+#name="Singh"     # Assigned new value to this variable. 
+echo "${name}"    # If commented last line, o/p will be blank, else, o/p will be "Singh"
