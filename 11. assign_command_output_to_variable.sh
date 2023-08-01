c281d868fc4cdd7dc6efdd0042f8c44a06cf3099
ABHAY4321 <85047246+ABHAY4321@users.noreply.github.com>
2023-08-01 06:49:28 +0530

    Update 11. assign_command_output_to_variable.sh

--- 11. assign_command_output_to_variable.sh
+++ 11. assign_command_output_to_variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Current_Working_Dir="pwd"
+#Current_Working_Dir="pwd"    # Here "pwd" will be print as it is. It will be consider as string, rather than command.
 
 #Current_Working_Dir=`pwd` #Old Way
 
