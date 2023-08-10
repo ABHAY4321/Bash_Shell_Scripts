6b07493956303859a67fdc259de0f1143b472445
ABHAY4321 <85047246+ABHAY4321@users.noreply.github.com>
2023-08-10 07:57:37 +0530

    Update 15. default_value.sh

--- 15. default_value.sh
+++ 15. default_value.sh
@@ -6,9 +6,9 @@ name=${name:-World} # Default value if no input given.
 
 echo "Hello ${name^}"
 
-yourname=${unsetvariable-Singh}     # If variable is unset, the "-" will set variable default value.
+yourname=${unsetvariable-Singh}     # If variable is unsetvariable, the "-" will set variable default value.
 echo "${yourname}"
 
 myname=""
-mytestname=${myname:-Pinku}         # If variable is unset or its value is an empty string, the ":-" will set variable default value.
+mytestname=${myname:-Pinku}         # If variable is unset (See first example of Hello World i.e no value passed to it) or its value is an empty string, the ":-" will set variable default value.
 echo "${mytestname}"
