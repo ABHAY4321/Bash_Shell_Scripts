745b4b4edfc10640b2d4581b516034fb5a0d8000
ABHAY4321 <85047246+ABHAY4321@users.noreply.github.com>
2024-06-24 05:08:50 -0400

    Update 04. install_pkg_by_args.sh

--- TWS-Shell Scripts/04. install_pkg_by_args.sh
+++ TWS-Shell Scripts/04. install_pkg_by_args.sh
@@ -7,3 +7,18 @@ sudo apt install ${1} -y
 echo "######################################"
 echo "Package ${1} is installed successfully"
 echo "######################################"
+
+===========================================================================
+===========================================================================
+#!/bin/bash
+echo "#################################"
+echo "Total no. of package to be installed: $#"
+echo "#################################"
+echo "The packages to be installed are: $@"
+echo "#################################"
+echo "Install the package ${1}"
+echo "#################################"
+sudo apt-get install ${1} -y
+echo "#################################"
+echo "Package ${1} has been installed successfully!"
+echo "#################################"
