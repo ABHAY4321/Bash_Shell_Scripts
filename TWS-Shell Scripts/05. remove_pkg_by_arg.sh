1c39f8cdf3d449f6c04233316ed5f6e0e6752aca
ABHAY4321 <85047246+ABHAY4321@users.noreply.github.com>
2024-06-24 05:00:57 -0400

    Update 05. remove_pkg_by_arg.sh

--- TWS-Shell Scripts/05. remove_pkg_by_arg.sh
+++ TWS-Shell Scripts/05. remove_pkg_by_arg.sh
@@ -10,3 +10,18 @@ sudo apt remove ${1} ${2} -y
 echo "######################################"
 echo "Package ${1} ${2} are uninstalled successfully"
 echo "######################################"
+
+=====================================================================================
+=====================================================================================
+#!/bin/bash
+echo "#################################"
+echo "Total no. of package to be removed: $#"
+echo "#################################"
+echo "The packages to be removed are: $@"
+echo "#################################"
+echo "Remove the package ${1}"
+echo "#################################"
+sudo apt-get remove ${1} -y && sudo apt autoremove -y
+echo "#################################"
+echo "Package ${1} has been removed successfully!"
+echo "#################################"
