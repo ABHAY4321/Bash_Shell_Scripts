76b0b4dcad4c4d340dec26643f0a0ba062ac4fd8
ABHAY4321 <85047246+ABHAY4321@users.noreply.github.com>
2024-11-04 07:27:25 +0530

    Update 02. variables.sh

--- TWS-Shell Scripts/02. variables.sh
+++ TWS-Shell Scripts/02. variables.sh
@@ -14,4 +14,19 @@ read -p "What is your channel name: " channel
 
 echo "Please subscribe my channel ${channel}"
 
+=============================================================================================================
+#!/bin/bash
 
+name="Abhay Kumar"
+echo "My name is ${name}"
+
+echo "Enter your Youtube Channel name: "
+read channel
+echo "Please subscribe to our channel ${channel} using your account credential as shown below-"
+
+read -p "Enter your username: " username
+read -p "Enter your password: " -s password
+
+echo -e "\nUsername: ${username}"
+echo "Password: ${password}"
+===============================================================================================================
