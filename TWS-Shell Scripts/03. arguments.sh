bcf750789f2613c974f8b75ba409e0b823b88516
ABHAY4321 <85047246+ABHAY4321@users.noreply.github.com>
2024-11-04 07:43:30 +0530

    Update 03. arguments.sh

--- TWS-Shell Scripts/03. arguments.sh
+++ TWS-Shell Scripts/03. arguments.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "This is $1"
+echo "My name is $1 $2 $3"
+echo "Total no. of passed arguments: $#"
+echo "Name the passed arguments: $@"
+
+======================================================================================
+#!/bin/bash
+
+echo "My name is $@" # No need to mention $1, $2 etc. It will cover all the passed arguments. Better method than previous one.
+echo "Total no. of passed arguments: $#"
+echo "Name the passed arguments: $@"
+
