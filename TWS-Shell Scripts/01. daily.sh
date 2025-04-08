8a86f2254149b3b60f89bf01eb3f0c8af608d3b6
ABHAY4321 <85047246+ABHAY4321@users.noreply.github.com>
2025-04-08 07:01:35 +0530

    Update 01. daily.sh

--- TWS-Shell Scripts/01. daily.sh
+++ TWS-Shell Scripts/01. daily.sh
@@ -7,4 +7,4 @@ echo "Today's Date: $(date | awk '{print $3,$2,$7}')"
 echo "Current Time: $(date | awk '{print $4,$5,$6}')"
 echo "Today's Day: $(date +%A)"
 echo "Memory Usage: $(free -h)"
-
+echo "Current Date/Time: $(date +%d/%m/%Y-%H:%M:%S)"
